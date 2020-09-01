d45566f7700a07469047fc62bd7e0cf03122b390
Luke Sanwick <lsanwick@gmail.com>
2020-09-01 13:54:35 -0700

    Fix requires

--- lib/apollo-studio-tracing.rb
+++ lib/apollo-studio-tracing.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'apollo-studio-tracing/proto'
-require 'apollo-studio-tracing/node_map'
-require 'apollo-studio-tracing/tracer'
+require 'apollo-studio-tracing/tracing/proto'
+require 'apollo-studio-tracing/tracing/node_map'
+require 'apollo-studio-tracing/tracing/tracer'
 require 'apollo-studio-tracing/tracing'
