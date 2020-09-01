7a63f1ad1743baabd0796c61d25c09a601784228
Luke Sanwick <lsanwick@gmail.com>
2020-09-01 14:37:08 -0700

    [Bugfix] Update URL for Apollo .proto

--- bin/generate-protos.sh
+++ bin/generate-protos.sh
@@ -9,7 +9,7 @@ echo "Removing old client"
 rm -f $OUTPUT_DIR/apollo.proto $OUTPUT_DIR/apollo_pb.rb
 
 echo "Downloading latest Apollo Protobuf IDL"
-curl --silent --output lib/apollo-studio-tracing/tracing/proto/apollo.proto https://raw.githubusercontent.com/apollographql/apollo-server/master/packages/apollo-engine-reporting-protobuf/src/reports.proto
+curl --silent --output lib/apollo-studio-tracing/tracing/proto/apollo.proto https://raw.githubusercontent.com/apollographql/apollo-server/main/packages/apollo-engine-reporting-protobuf/src/reports.proto
 
 echo "Generating Ruby client stubs"
 protoc -I lib/apollo-studio-tracing/tracing/proto --ruby_out lib/apollo-studio-tracing/tracing/proto lib/apollo-studio-tracing/tracing/proto/apollo.proto
